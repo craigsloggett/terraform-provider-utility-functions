d4871cb1637adbb1426d5b6179544853ccbcfef3
Craig Sloggett <craigsloggett@gmail.com>
2025-11-20 14:41:02 -0500

    feat: Add a random string generator function (#91)
    This pull request adds a random string generator function with a
    required `length` input parameter.

--- examples/functions/outputs.tf
+++ examples/functions/outputs.tf
@@ -1,5 +1,11 @@
-output "example" {
+output "get_env" {
   # value     = provider::utilities::get_env("GOPATH")
   description = "The value of a given environment variable."
+  value       = "Uncomment output value to test provider function." # TFLint doesn't yet support provider-defined functions.
+}
+
+output "generate_random_string" {
+  # value     = provider::utilities::generate_random_string(10)
+  description = "A random number with a specified length."
   value       = "Uncomment output value to test provider function." # TFLint doesn't yet support provider-defined functions.
 }
